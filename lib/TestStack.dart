59461d390329bf4f9c998c03e0f082ea6571db7c
AbdElhamid-Zyada <ab.zy.az30@gmail.com>
2022-11-18 15:38:46 +0200

    adding padding and border raduis to stack

--- lib/TestStack.dart
+++ lib/TestStack.dart
@@ -7,21 +7,37 @@ class TestStack extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Stack(
-        alignment: AlignmentDirectional.bottomEnd,
-        children: [
-          Image(
-              image: NetworkImage(
-                  "https://pps.whatsapp.net/v/t61.24694-24/243223815_1119082322063194_4334476928733186335_n.jpg?ccb=11-4&oh=01_AdR1qEKRjTePqdXENyYKH0gMpQhfStTfTkw0YoGWjMp0Jw&oe=637F3DC6")),
-          Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-            Text(
-              "Abd El-hamed Zyada",
-              style: TextStyle(
-                backgroundColor: Colors.black12,
-              ),
+      body: Padding(
+        padding: const EdgeInsetsDirectional.all(10),
+        child: Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadiusDirectional.only(
+              topStart: Radius.circular(15),
+              bottomEnd: Radius.circular(15),
             ),
-          ]),
-        ],
+          ),
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          child: Stack(
+            alignment: AlignmentDirectional.bottomEnd,
+            children: [
+              const Image(
+                  image: NetworkImage(
+                      "https://pps.whatsapp.net/v/t61.24694-24/243223815_1119082322063194_4334476928733186335_n.jpg?ccb=11-4&oh=01_AdR1qEKRjTePqdXENyYKH0gMpQhfStTfTkw0YoGWjMp0Jw&oe=637F3DC6")),
+              Container(
+                color: Colors.amber,
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  const Text(
+                    "Abd El-hamed Zyada",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ]),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
